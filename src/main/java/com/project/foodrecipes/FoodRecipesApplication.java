fae08a6e105a40465334f5d6516f2a90948a46c8
lucianommatos <lucianommatos@gmail.com>
2020-09-09 12:26:53 +0100

    Initial project commit

---
+++ src/main/java/com/project/foodrecipes/FoodRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.project.foodrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRecipesApplication.class, args);
+	}
+
+}
