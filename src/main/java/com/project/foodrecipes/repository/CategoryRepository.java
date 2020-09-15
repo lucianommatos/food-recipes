cb342f5f7295d8238dbf8cd766e01f412b735deb
lucianommatos <lucianommatos@gmail.com>
2020-09-15 12:55:24 +0100

    Added Spring Data JPA method example

--- src/main/java/com/project/foodrecipes/repository/CategoryRepository.java
+++ src/main/java/com/project/foodrecipes/repository/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.project.foodrecipes.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.project.foodrecipes.model.Category;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
+	Optional<Category> findByDescription(String description);
+	
 }
--- src/main/java/com/project/foodrecipes/repository/UnitOfMeasureRepository.java
+++ src/main/java/com/project/foodrecipes/repository/UnitOfMeasureRepository.java
@@ -1,9 +1,13 @@
 package com.project.foodrecipes.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.project.foodrecipes.model.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+	
+	Optional<UnitOfMeasure> findByDescription(String description);	
 
 }
